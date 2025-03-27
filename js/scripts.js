83f7aaa8ea6e28c0f06552d09f2fd7a722418d77
sabts <sabrinataraschi@gmail.com>
2025-03-27 16:18:18 +0100

    Update scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+//Ejercicios de Objetos con Arrays, Bucles, Strings, Condicionales y Lógica
+
+//1️⃣ Macarena y la validación de contraseñas:
+// Crea una función validatePassword que reciba un objeto con una propiedad password (string) y devuelva un objeto con las propiedades isValid (boolean) y errors (array de strings) que indiquen si la contraseña cumple con los siguientes criterios:
+
+const validatedPassword ={
+password:[],
+error: [],
+}
+
+//Longitud mínima de 8 caracteres
+//Al menos una letra mayúscula
+// Al menos una letra minúscula
+// Al menos un número
+// Ejemplo entrada: { password: "abc123" }
+// Ejemplo salida: {isValid: false, y tipo de error}
+const validatePassword =()=>{
+
+}
